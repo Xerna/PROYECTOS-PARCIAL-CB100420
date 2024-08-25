9f22598629a6c5b6b9ad156189e5dadfe62301e3
Ricardo Cerna <ricardo.cerna2001@gmail.com>
2024-08-24 22:15:33 -0600

    Initial commit

---
+++ MiParcialitoB/MiParcialitoB/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiParcialitoB.Models;
+
+namespace MiParcialitoB.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Inscripcion> Inscripciones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Inscripcion>()
+                .HasKey(i => new { i.EstudianteID, i.CursoID });
+
+            modelBuilder.Entity<Inscripcion>()
+                .HasOne(i => i.Estudiante)
+                .WithMany(e => e.Inscripciones)
+                .HasForeignKey(i => i.EstudianteID);
+
+            modelBuilder.Entity<Inscripcion>()
+                .HasOne(i => i.Curso)
+                .WithMany(c => c.Inscripciones)
+                .HasForeignKey(i => i.CursoID);
+        }
+    }
+}
---
+++ MiParcialitoC/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiParcialitoC.Models;
+
+namespace MiParcialitoC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Welcome(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(userModel);
+            }
+
+            // Si el modelo no es válido, vuelve a mostrar el formulario.
+            return View("Index");
+        }
+    }
+}
